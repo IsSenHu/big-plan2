7ba4308cffded43df79a4f945949fbfa22512950
IsSenHu <1178515826@qq.com>
2021-02-11 21:34:55 +0800

    修改redis nacos mysql配置

--- docker/mysql.sh
+++ docker/mysql.sh
@@ -1,2 +1,2 @@
 mkdir /data/mysql
-docker run --name my-mysql -v /data/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=521428Slyt! -d mysql:5.7.33 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+docker run --name my-mysql -v /data/mysql:/var/lib/mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=521428Slyt! -d mysql:5.7.33 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
